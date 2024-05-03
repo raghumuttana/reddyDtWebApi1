cf7f32098e840da56760b6b6e747a060d73e7023
rreddy <r.reddy@outlook.com>
2024-05-03 10:14:02 -0500

    test commit

--- webApi1/Program.cs
+++ webApi1/Program.cs
@@ -41,4 +41,5 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    //comment for test commit
 }
