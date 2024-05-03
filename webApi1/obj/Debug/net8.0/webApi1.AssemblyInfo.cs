cd12a3da6c2db64d3f885fee579a625d7edf67d1
rreddy <r.reddy@outlook.com>
2024-05-03 10:17:17 -0500

    config changes

--- webApi1/obj/Debug/net8.0/webApi1.AssemblyInfo.cs
+++ webApi1/obj/Debug/net8.0/webApi1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webApi1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f42019c5bfcb3dfee3d65a1b6729644f07d7441")]
 [assembly: System.Reflection.AssemblyProductAttribute("webApi1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webApi1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
